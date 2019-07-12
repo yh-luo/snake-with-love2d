27cbb1c31a63e6e7f47e0490539aadff5cb4b19e
Yu-Han <yuhanluo1994@gmail.com>
2019-07-13 01:01:56 +0800

    new way of drawing the snake

--- main.lua
+++ main.lua
@@ -2,20 +2,19 @@ WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 1024
 TILE_SIZE = 32
 
-MAX_TILES_X = math.floor(WINDOW_WIDTH/TILE_SIZE)
-MAX_TILES_Y = math.floor(WINDOW_HEIGHT/TILE_SIZE)
+MAX_TILES_X = math.floor(WINDOW_WIDTH / TILE_SIZE)
+MAX_TILES_Y = math.floor(WINDOW_HEIGHT / TILE_SIZE)
 
 -- time the snake moves one tile
 SNAKE_SPEED = 0.1
 
 -- because index starts from 1 in Lua
-local snakeX, snakeY = nil, nil -- unneccessary but nice practice to keep variables local
--- local tileGrid = {}
+-- local snakeX, snakeY = nil, nil -- unneccessary but nice practice to keep variables local
 local snakeBody = {}
 local score = 0
 local snakeTimer = 0
-local appleX, appleY = nil, nil -- unneccessary but nice practice to keep variables local
-
+local apple = {}
+local direction = {0, 0}
 
 function love.load()
     love.window.setTitle('Snake')
@@ -23,8 +22,8 @@ function love.load()
     love.graphics.setFont(love.graphics.newFont(32))
     math.randomseed(os.time())
     -- randomly generate the snake
-    snakeX = math.random(MAX_TILES_X-1)
-    snakeY = math.random(MAX_TILES_Y-1)
+    table.insert(snakeBody,
+                 {math.random(MAX_TILES_X-1), math.random(MAX_TILES_Y-1)})
     -- create an apple
     createApple()
 end
@@ -33,7 +32,7 @@ function love.update(dt)
     snakeTimer = snakeTimer + dt
 
     if snakeTimer >= SNAKE_SPEED then
-        if snakeX == appleX and snakeY == appleY then
+        if snakeBody[1][1] == apple[1] and snakeBody[1][2] == apple[2] then
             score = score + 1
             -- create an new apple
             createApple()
@@ -46,7 +45,6 @@ function love.update(dt)
 
 end
 
-
 function love.draw()
     drawScore()
     drawApple()
@@ -54,35 +52,36 @@ function love.draw()
 end
 
 function love.keypressed(key)
-    if key == 'escape' then
-        love.event.quit()
-    end
+    if key == 'escape' then love.event.quit() end
 
     if key == 'left' then
-        snakeMoving = 'left'
+        direction = {-1, 0}
     elseif key == 'right' then
-        snakeMoving = 'right'
+        direction = {1, 0}
     elseif key == 'up' then
-        snakeMoving = 'up'
+        direction = {0, -1}
     elseif key == 'down' then
-        snakeMoving = 'down'
+        direction = {0, 1}
     end
 end
 
 function createApple()
-    appleX = math.random(MAX_TILES_X-1)
-    appleY = math.random(MAX_TILES_Y-1)
+    apple = {math.random(MAX_TILES_X-1), math.random(MAX_TILES_Y-1)}
 end
 
 function drawSnake()
-    -- draw the head
     love.graphics.setColor(0, 1, 0)
-    love.graphics.rectangle('fill', snakeX*TILE_SIZE, snakeY*TILE_SIZE, TILE_SIZE, TILE_SIZE)
+    -- draw the snake
+    for i = 1, #snakeBody do
+        love.graphics.rectangle('fill', snakeBody[i][1]*TILE_SIZE, snakeBody[i][2]*TILE_SIZE,
+                                TILE_SIZE, TILE_SIZE)
+    end
 end
 
 function drawApple()
     love.graphics.setColor(1, 0, 0)
-    love.graphics.rectangle('fill', appleX*TILE_SIZE, appleY*TILE_SIZE, TILE_SIZE, TILE_SIZE)
+    love.graphics.rectangle('fill', apple[1]*TILE_SIZE, apple[2]*TILE_SIZE,
+                            TILE_SIZE, TILE_SIZE)
 end
 
 -- show the score
@@ -92,24 +91,16 @@ function drawScore()
 end
 
 function moveSnake()
-    if snakeMoving == 'left' then
-        snakeX = snakeX - 1
-    elseif snakeMoving == 'right' then
-        snakeX = snakeX + 1
-    elseif snakeMoving == 'up' then
-        snakeY = snakeY - 1
-    elseif snakeMoving == 'down' then
-        snakeY = snakeY + 1
-    end
+    snakeBody[1] = {snakeBody[1][1]+direction[1], snakeBody[1][2]+direction[2]}
     -- check if the snake is out of the boundaries
-    if snakeX > MAX_TILES_X then
-        snakeX = 1
-    elseif snakeX < 1 then
-        snakeX = MAX_TILES_X
+    if snakeBody[1][1] > MAX_TILES_X then
+        snakeBody[1][1] = 0
+    elseif snakeBody[1][1] < 1 then
+        snakeBody[1][1] = MAX_TILES_X
     end
-    if snakeY > MAX_TILES_Y then
-        snakeY = 1
-    elseif snakeY < 1 then
-        snakeY = MAX_TILES_Y
+    if snakeBody[1][2] > MAX_TILES_Y then
+        snakeBody[1][2] = 0
+    elseif snakeBody[1][2] < 1 then
+        snakeBody[1][2] = MAX_TILES_Y
     end
 end
