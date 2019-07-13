2b0d803f1b1c60139d268afd49ca16c6d96b41aa
Yu-Han Luo <yuhanluo1994@gmail.com>
2019-07-13 15:02:18 +0800

    add collision detection

--- main.lua
+++ main.lua
@@ -5,49 +5,58 @@ TILE_SIZE = 32
 MAX_TILES_X = math.floor(WINDOW_WIDTH / TILE_SIZE)
 MAX_TILES_Y = math.floor(WINDOW_HEIGHT / TILE_SIZE)
 
+LEFT = {-1, 0}
+RIGHT = {1, 0}
+UP = {0, -1}
+DOWN = {0, 1}
+
 -- time the snake moves one tile
 SNAKE_SPEED = 0.1
-math.randomseed(os.time())
--- randomly generate the snake
-local snakeBody = {{math.random(MAX_TILES_X-1), math.random(MAX_TILES_Y-1)}}
-local score = 0
-local snakeTimer = 0
-local apple = {}
-local direction = {0, 0}
+
+local snakeBody, score, snakeTimer, apple, direction, endingTimer = nil
+local ending = false
 
 function love.load()
     love.window.setTitle('Snake')
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
     love.graphics.setFont(love.graphics.newFont(32))
-
-    -- create an apple
-    createApple()
+    
+    snakeReset()
 end
 
 function love.update(dt)
-    snakeTimer = snakeTimer + dt
-
-    if snakeTimer >= SNAKE_SPEED then
-        -- moving
-        moveSnake()
-        if snakeBody[#snakeBody][1] == apple[1] and snakeBody[#snakeBody][2] == apple[2] then
-            score = score + 1
-            -- update the snake
-            table.insert(snakeBody, 1, {snakeBody[1][1]-direction[1], snakeBody[1][2]-direction[2]})
-            -- create an new apple
-            createApple()
+    if not snakeColide() then
+        snakeTimer = snakeTimer + dt
+        if snakeTimer >= SNAKE_SPEED then
+            if table.concat(snakeBody[#snakeBody]) == table.concat(apple) then
+                score = score + 1
+                -- update the snake
+                table.insert(snakeBody, 1, {snakeBody[1][1]-direction[1], snakeBody[1][2]-direction[2]})
+                -- create an new apple
+                createApple()
+            end
+            -- moving
+            moveSnake()
+            -- reset the timer
+            snakeTimer = 0
         end
-
-        -- reset the timer
-        snakeTimer = 0
+    else
+        -- create a timer for ending
+        endingTimer = endingTimer + dt
+        if endingTimer >= 2 then
+            snakeReset()
+        end
     end
-
 end
 
 function love.draw()
-    drawScore()
-    drawApple()
-    drawSnake()
+    if not snakeColide() then
+        drawScore()
+        drawApple()
+        drawSnake()
+    else
+        drawEnding()
+    end
 end
 
 function love.keypressed(key)
@@ -55,15 +64,41 @@ function love.keypressed(key)
         love.event.quit()
     end
 
-    if key == 'left' then
-        direction = {-1, 0}
-    elseif key == 'right' then
-        direction = {1, 0}
-    elseif key == 'up' then
-        direction = {0, -1}
-    elseif key == 'down' then
-        direction = {0, 1}
+    if #snakeBody == 1 then
+        if key == 'left' then
+            direction = LEFT
+        elseif key == 'right' then
+            direction = RIGHT
+        elseif key == 'up' then
+            direction = UP
+        elseif key == 'down' then
+            direction = DOWN
+        end
+    else
+        if key == 'left' and table.concat(direction) ~= table.concat(RIGHT) then
+                direction = LEFT
+        elseif key == 'right' and table.concat(direction) ~= table.concat(LEFT) then
+                direction = RIGHT
+        elseif key == 'up' and table.concat(direction) ~= table.concat(DOWN) then
+                direction = UP
+        elseif key == 'down' and table.concat(direction) ~= table.concat(UP) then
+                direction = DOWN
+        end
     end
+end
+
+function snakeReset()
+    -- randomly generate the snake
+    math.randomseed(os.time())
+    snakeBody = {{math.random(MAX_TILES_X-1), math.random(MAX_TILES_Y-1)}}
+    score = 0
+    snakeTimer = 0
+    apple = {}
+    direction = {0, 0}
+    ending = false
+    endingTimer = 0
+    -- create an apple
+    createApple()
 end
 
 function appleInSnake()
@@ -106,12 +141,16 @@ function drawScore()
     love.graphics.print('Score: ' .. tostring(score), 10, 10)
 end
 
+function drawEnding()
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print('Score: ' .. tostring(score), 640, 512)
+end
+
 function moveSnake()
-    snakeBody[#snakeBody] = {snakeBody[#snakeBody][1]+direction[1], snakeBody[#snakeBody][2]+direction[2]}
-    -- FIXME: when the score is 1, the snake does not increament its length
     for i = 1, #snakeBody-1 do
         snakeBody[i] = snakeBody[i+1]
     end
+    snakeBody[#snakeBody] = {snakeBody[#snakeBody][1]+direction[1], snakeBody[#snakeBody][2]+direction[2]}
     for i = 1, #snakeBody do
         -- check if the snake is out of the boundaries
         if snakeBody[i][1] > MAX_TILES_X then
@@ -128,4 +167,15 @@ function moveSnake()
 
 end
 
--- TODO: check if the snake bump into itself
+-- check if the snake bump into itself
+function snakeColide()
+    if #snakeBody > 1 then
+        for i = 1, #snakeBody-1 do
+            if table.concat(snakeBody[i]) == table.concat(snakeBody[#snakeBody]) then
+                return true
+            end
+        end
+    else
+        return false
+    end
+end
